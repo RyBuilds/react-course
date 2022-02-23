066d99202aa945929acf40649e24ac9ec8e1f655
RyBuilds <Ryan@Registe.me>
2022-02-23 23:26:53 +0000

    props, state, event props, functions as props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 const Todo = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+        {modalIsOpen && <Modal onCancel={closeModalHandler} />}
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
       </div>
     </div>
   );
