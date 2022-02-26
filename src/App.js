6491cff4658cadab3693a39665b691950c7b2159
RyBuilds <Ryan@Registe.me>
2022-02-26 00:34:47 +0000

    styling, outputting lists of data, wrapper component, props.children

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./Pages/AllMeetups";
 import NewMeetupPage from "./Pages/NewMeetup";
 import FavouritesPage from "./Pages/Favourites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route exact path="/" component={AllMeetupsPage} />
         <Route path="/new-meetup" component={NewMeetupPage} />
         <Route path="/favourites" component={FavouritesPage} />
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
